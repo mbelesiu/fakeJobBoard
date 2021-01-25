2d7152ede6a60765bddb2d91f58c1e69cd76ad29
mbelesiu <matt.belesiu@gmail.com>
2021-01-25 09:56:28 -0800

    init files and templates

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+function App() {
+  return(<div>Hello from React</div>);
+}
+
+
+export default App;
